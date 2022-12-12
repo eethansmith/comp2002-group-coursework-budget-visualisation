f668e526b1618319efbbb91ee9ae2d39aebca1aa
Tom McCracken <108665428+myzonetom@users.noreply.github.com>
2022-12-12 21:35:29 +0000

    finished header css
    add state so the css will stick when a buttons been pressed
    
    aswell as this i added in two different objects in the graph so its clear
    when a buttons been pressed.

--- src/App.js
+++ src/App.js
@@ -6,14 +6,22 @@ import { IoSwapHorizontalOutline } from 'react-icons/io5'
 
 const App = () => {
 
-  const [isRingChart, setIsRingChart] = useState(0)
+  const [isRingChart, setIsRingChart] = useState(0);
+  const[isDaily, setDaily] = useState(true);
 
+  const updateDaily = (bool) => {
+    setDaily(bool);
+  }
 
+  const dailyObject = {Groceries:12, Transport:13 ,Entertainment:4, Other:6}
+  const monthlyObject = {Groceries:72, Transport:30 ,Entertainment:64, Other:100}
+
+  
   return (
     <>
-      <Header></Header>
+      <Header updateDaily={updateDaily} isDaily={isDaily}></Header>
       <button onClick={() => {setIsRingChart(!isRingChart)}}><IoSwapHorizontalOutline /></button>
-      <Chart isRingChart={isRingChart} data={{Groceries:12, Transport:13 ,Entertainment:4, Other:6}} height="200" width="200" />
+      <Chart isRingChart={isRingChart} data={isDaily ? dailyObject: monthlyObject} height="200" width="200" />
     </>
   );
 }
