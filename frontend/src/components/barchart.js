99e685a9aa86e1963b54bc3c55769dedecb5f4e0
psydv2 <psydv2@nottingham.ac.uk>
2023-02-23 23:36:15 +0000

    feat: made the number of axes displayed on the bar chart easier to parse for visualising data.

--- frontend/src/components/barchart.js
+++ frontend/src/components/barchart.js
@@ -1,4 +1,6 @@
-const colorPallete = ['#7D2B54', '#9E4B95', '#BF6C78', '#DACD82', '#97CAEB', '#61A89A', '#37753B', '#312383']
+const colorPallete = 
+    ['#7D2B54', '#9E4B95', '#BF6C78', '#DACD82',
+     '#97CAEB', '#61A89A', '#37753B', '#312383']
 
 const BarChart = (props) => {
 
@@ -82,7 +84,8 @@ function getYLabelValues(height, data){
     const maxValue = Math.max(...valuesArray);
     const multiplier = height / maxValue;
     const yLabels = [];
-    var yLabelValues = pushLines(maxValue);
+    
+    let yLabelValues = pushLines(maxValue);
 
     yLabelValues.forEach((value, index) => {
         yLabels.push(
@@ -111,7 +114,7 @@ function getLines(height, data) {
 
     yLabelValues = pushLines(maxValue);
 
-    var lineOpacity = "1";
+    let lineOpacity = "1";
 
     yLabelValues.forEach((value, index) => {
 
@@ -131,7 +134,7 @@ function getLines(height, data) {
                 opacity = {lineOpacity}
             />
         )
-        lineOpacity = "0.5";
+        lineOpacity = "0.3";
     })
 
     return lines;
@@ -139,21 +142,54 @@ function getLines(height, data) {
 
 function pushLines(maxValue) {
 
-    var temp = maxValue;
-    var orderOfMagnitude = 0;
+    let tempMaxVal = maxValue;
+    let orderOfMagnitude = 0;
+    let lineModifier = 1;
 
     const axesLabels = [];
 
-    while( temp > 10 ) {
-        temp = temp / 10;
+    while( tempMaxVal > 10 ) {
+        tempMaxVal = tempMaxVal / 10;
         orderOfMagnitude++;
     }
 
-    const numLines = maxValue / (10 ** orderOfMagnitude) + 1 ;
+    const calcNumLines = (maxValue / (10 ** orderOfMagnitude));
+    
+    /*
+        In the if statement below are the values that determine how many axes 
+        can appear on the graph. These are values that could change later but 
+        for the time being they work.
 
-    for (var i = 0; i < numLines; i++) {
+        I am using strictly less/greater than symbols as calcNumLines is unlikely to be
+        a whole number.
+    */
+    if( calcNumLines > 7 ) {
+        lineModifier *= 2;
+    } else if ( calcNumLines < 2 ) {
+        lineModifier *= 1/5;
+    } else if ( calcNumLines < 3 ) {
+        lineModifier *= 1/2;
+    }
+
+    let iterations = calcNumLines/lineModifier;
+    const denominator = (10 ** orderOfMagnitude) * lineModifier;
+
+    /* 
+        The below if statement checks whether the difference between the 
+        max value and the top axis value is greater than half of the 
+        difference between axes.
+
+        If it is, then it adds another axis line above the bars. This way the axes
+        shouldn't ever go over the limits of the svg, and still provide a better 
+        visualisation of the spending.
+    */ 
+    if ( (maxValue % denominator) / denominator > 0.5 ) {
+        iterations++;
+    }
+
+    for (let i = 0; i < iterations; i++) {
         axesLabels.push(
-            i*(10 ** orderOfMagnitude)
+            i * (lineModifier) * (10 ** (orderOfMagnitude))
         )
     }
 
