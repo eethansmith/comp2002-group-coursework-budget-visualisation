b7fb1126dce49499525d19ace8a7ae05f697a3d8
psydv2 <psydv2@nottingham.ac.uk>
2023-03-03 15:20:27 +0000

    chore: pushing for paired programming

--- frontend/src/components/budgetchart.js
+++ frontend/src/components/budgetchart.js
@@ -1,8 +1,38 @@
 
-function BudgetChart() {
+const BudgetChart = ((props) => {
+    
+    const HEIGHTWIDTH = 500;
+
+    const spendingData = getSpendingData(HEIGHTWIDTH, props.data);
+
+    const bars = [];
+    const textBoxes = [];
+    
+
 
     return (
         <>
+            <svg className="budgetChart Chart" viewBox="0 0 500 500">
+                {bars}
+                {axis}
+                {budgetLine}
+            </svg>
         </>
     )
+})
+
+function getSpendingData(width, data) {
+    const valuesArray = Object.values(data);
+    const maxValue = Math.max(...valuesArray);
+    const multiplier = height / maxValue;
+    
+    let temp = {};
+
+    Object.keys(data).forEach((key, index)=> {
+        temp[key] = data[key]*multiplier;
+    })
+
+    return temp;
 }
+
+export default BudgetChart;
