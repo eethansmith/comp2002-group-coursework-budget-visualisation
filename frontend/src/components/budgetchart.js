0d11ee71d344bbb4867f60268a65b4dc2f96e905
Daniel Valadkevich <danielvaladkevich@gmail.com>
2023-03-29 18:40:37 +0100

    chore: comments all over the budgetchart component

--- frontend/src/components/budgetchart.js
+++ frontend/src/components/budgetchart.js
@@ -13,18 +13,37 @@ const BudgetChart = ((props) => {
     const axesStart = HEIGHTWIDTH/10 + (HEIGHTWIDTH/20)/2;
     const axesEnd = (9*HEIGHTWIDTH)/10 - (HEIGHTWIDTH/20)/2;
     
+    // All bars are going to appear in this range
+    // axesStart is where the top of the top bar will start getting drawn at
+    // axesEnd is where the bottom of the bars finish getting drawn
+    // NOTE: There will usually be a sliver of empty space between the marker (e.g. axesStart) and the ends of a bar
+    //       This is due to my personal desire (Dan) for the bars to appear about the exact centre of the axes
     const axesTotal = axesEnd - axesStart;
 
+    // barSpacingUtil is the actual difference between the top left of the adjacent bars in the array
     const barSpacingUtil = axesTotal/4;
+
+    // barHeight is the actual height of the bars 
+    // In other words barSpacingUtil - barHeight = distance between bars
+    // NOTE: The offset that centres the bars about the centre of the axes is half of (1 - this multiplier)
+    //       Basically if you don't add that offset and have this as your barheight, the bars will be at that much of an offset
+    //       To understand this intuitively draw out the maths of these bars yourself, I think there's actually a design of that
+    //           for the barChart component in figma.
     const barHeight = barSpacingUtil*(7/8);
 
+    // A static line that displays how much you've spent in comparison to how much you've planned to spend this month according to your salary
     const budgetLinePos = 0.7 * HEIGHTWIDTH;
+
+    // Position of yAxis line (leftmost line that bars come from) relative to the size of the svg
     const yAxis = 0.1 * HEIGHTWIDTH;
 
+    // Initial values for the budgeting of the categories as a proportion of the inputted salary
     const proportionsOfSalary = [0.4, 0.15, 0.3];
 
+    // Distance between start of bar to the budget line, useful for the maths later
     const distanceToBudgetLine = budgetLinePos - yAxis;
 
+    // Object to store values from the props
     let spendingDataObject = {Bills:0,Groceries:0,Other:0};
 
     Object.keys(spendingDataObject).forEach((key, index) => {
@@ -33,9 +52,11 @@ const BudgetChart = ((props) => {
         }
     })
 
-    console.log(spendingDataObject);
+    // console.log(spendingDataObject); // Debug line to make sure the object is populated with data
+
     let spendingData = Object.values(spendingDataObject);
 
+    // Next two "push" statements are pushing the budgetline and the startline from the data
     yAxes.push(
         <line 
             x1={yAxis}
@@ -59,43 +80,40 @@ const BudgetChart = ((props) => {
             className="budgetLine"
         />
     )
-    
-    // TODO: push a line be the "budget line"
-        // we will use this line as the 100% point for the bars - i havent thought about this implementation yet
 
     // TODO: forms/text fields here -> theres a static number of them so it shouldnt be too bad.
 
-    // TODO: Bar locations/dimensions
-    // forEach category 
-        // barWidth = ???
-        // Xcoord = yAxis
-        // Ycoord(topLeft) = axesStart + ((index + 1/16) * barSpacingUtil )
-        // push to bars array
-
-
+    // Drawing the bars to the chart
     Object.keys(spendingData).forEach((key,index) => {
         bars.push(
          <rect 
-            x={yAxis}
+            x={yAxis} // position of the leftmost point of the bar
             y={
-                (barSpacingUtil*(index + 1/16)) + axesStart
+                (barSpacingUtil*(index + 1/16)) + axesStart // position of the top of the bar
             }
             height={barHeight}
             width={
-                (spendingData[key]/(proportionsOfSalary[index] * salary)) * (distanceToBudgetLine)
+                (spendingData[key]/(proportionsOfSalary[index] * salary)) * (distanceToBudgetLine) 
+                // The above line is some fun cool maths that figures out the proportion spent in comparison to the proportion budgeted
+                // An example makes this clearer; if you wanted to spend half your salary on groceries, but ended up spending 75% on groceries instead,
+                //     proportionally this is a 150% increase so the bar would span the distance to the budget line, and then half that same distance again.
             }
             fill='#97CAEB'
          />
         )
     })
 
-    let totalBudgeted = 0;
+
+    // This variable and the forEach statement below it are here to calculate how much of the salary you've spent.
+    let totalSpent = 0;
 
     Object.keys(spendingData).forEach((key, index) => {
-        totalBudgeted += spendingData[key];
-        console.log(totalBudgeted);
+        totalSpent += spendingData[key];
+        // console.log(totalBudgeted);
     })
 
+    // pushing the totalBar to the bars array
+    // Basically displays the proportion spent in comparison to the user's salary
     bars.push(
         <rect
             key="Total"
@@ -105,7 +123,7 @@ const BudgetChart = ((props) => {
             }
             height={barHeight}
             width={
-                (totalBudgeted/salary) * (distanceToBudgetLine)
+                (totalSpent/salary) * (distanceToBudgetLine)
             }
             fill='#1C2640'
         />
