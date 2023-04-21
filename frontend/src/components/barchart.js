b8b6678f3270afcfaa7b232dc061ef70305efdb0
Daniel Valadkevich <danielvaladkevich@gmail.com>
2023-04-21 16:13:22 +0300

    feat: refactored some const names in the barchart component to abide to the all caps const convention

--- frontend/src/components/barchart.js
+++ frontend/src/components/barchart.js
@@ -13,27 +13,27 @@ const BarChart = (props) => {
     const yLabels = getYLabelValues(HEIGHTWIDTH, props.data);
     const lines = getLines(HEIGHTWIDTH, props.data);
     
-    const dataLen = Object.keys(props.data).length;
+    const NUM_BARS = Object.keys(props.data).length;
 
     // These two variables determine where along the axes the bars will 
-    const axesStart = HEIGHTWIDTH/10 + (HEIGHTWIDTH/20)/2;
-    const axesEnd = (9*HEIGHTWIDTH)/10 - (HEIGHTWIDTH/20)/2;
+    const AXES_START = HEIGHTWIDTH/10 + (HEIGHTWIDTH/20)/2;
+    const AXES_END = (9*HEIGHTWIDTH)/10 - (HEIGHTWIDTH/20)/2;
 
-    const axesTotal = axesEnd - axesStart;
+    const TOTAL_AXES_LENGTH = AXES_END - AXES_START;
 
     // Pre-decided bar width and spacing based on axes width
-    const barSpacingUtil = axesTotal/(dataLen);
+    const BAR_SPACING_UTIL = TOTAL_AXES_LENGTH/(NUM_BARS);
 
-    const barWidth = barSpacingUtil*7/8;
+    const BAR_WIDTH = BAR_SPACING_UTIL*7/8;
 
     Object.keys(props.data).forEach((key, index) => {
         bars.push(
             <rect 
                 key={key} 
-                width={barWidth} 
+                width={BAR_WIDTH} 
                 height ={heightedData[key]*0.7} 
                 x={
-                    (barSpacingUtil*(index + 1/16)) + axesStart
+                    (BAR_SPACING_UTIL*(index + 1/16)) + AXES_START
                 } 
                 y={HEIGHTWIDTH*(1 - 0.15) - heightedData[key]*0.7} 
                 fill={colorPallete[index]} 
@@ -42,7 +42,7 @@ const BarChart = (props) => {
         xLabels.push(
             <text
                 key={key}
-                x={(HEIGHTWIDTH/dataLen)*index + (HEIGHTWIDTH/dataLen)/2}
+                x={(HEIGHTWIDTH/NUM_BARS)*index + (HEIGHTWIDTH/NUM_BARS)/2}
                 y={HEIGHTWIDTH*(1 - 0.05)}
                 textAnchor="middle"
                 fontSize="20"
@@ -107,12 +107,12 @@ function getYLabelValues(height, data){
 
 function getLines(height, data) {
     const valuesArray = Object.values(data);
-    const maxValue = Math.max(...valuesArray);
-    const multiplier = height / maxValue;
+    const MAX_VALUE = Math.max(...valuesArray);
+    const MULTIPLIER = height / MAX_VALUE;
     const lines = [];
     var yLabelValues = [];
 
-    yLabelValues = pushLines(maxValue);
+    yLabelValues = pushLines(MAX_VALUE);
 
     let lineOpacity = "1";
 
@@ -126,9 +126,9 @@ function getLines(height, data) {
             <line
                 key={index}
                 x1={height/10}
-                y1={height*(1 - 0.15) - value*multiplier*0.7}
+                y1={height*(1 - 0.15) - value*MULTIPLIER*0.7}
                 x2={9*height/10}
-                y2={height*(1 - 0.15) - value*multiplier*0.7}
+                y2={height*(1 - 0.15) - value*MULTIPLIER*0.7}
                 stroke="black"
                 strokeWidth="1"
                 opacity = {lineOpacity}
